c3fec2fa5062e853f1f64aad06210d0ee6f81654
JoseDJarias <arias_da044@hotmail.com>
2023-08-24 00:45:54 -0600

    v1

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            Hola EN ABOUT
+        </div>
+    );
+}
+
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import '../styles/header.css'
+import Banner from '../../src/assets/img/Banner.jpg'
+
+function Header() {
+    return (
+        <Router>
+            <>
+                <div className='banner'>
+                    <img src={Banner} alt='pokeBanner'
+                    />
+                </div>
+                <nav className='navigator'>
+                    <ul>
+                        <li><Link to="/">Inicio</Link> </li>
+                        <li><Link to="/about">About</Link> </li>
+                    </ul>
+                </nav>
+                <Routes>
+                    <Route path='/' element={<Home />}></Route>
+                    <Route path='/about' element={<About />}></Route>
+                </Routes>
+                <h2>Poke Page</h2>
+            </>
+        </Router>
+    );
+}
+
+export default Header;
